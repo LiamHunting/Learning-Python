c6ed6d2663a3d1a9f861c3f0e8dad738146a7b81
LiamHunting <hunting.liam@gmail.com>
2023-05-08 16:05:32 -0600

    small push

---
+++ python_basics/string_indexes.py
@@ -0,0 +1,14 @@
+selfish = 'me me me'
+#01234567
+
+print(selfish[0]) #m
+
+print(selfish[7]) #e
+
+#[start:stop]
+print(selfish[0:4]) #'me '
+
+#[start:stop:stepover]
+print(selfish[0:8:1]) #'me me me'
+print(selfish[0:8:2]) #'m em'
+print(selfish[::-1]) #'em em em'
